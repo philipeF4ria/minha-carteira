4166db8eb313bad7f29243abffd877d2249b71a4
Philipe Faria <philipefaria61@gmail.com>
2020-11-10 13:21:37 -0300

    MainAside styles finished

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import {
+	MdDashboard,
+	MdArrowUpward,
+	MdArrowDownward,
+	MdExitToApp,
+} from 'react-icons/md';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import {
+	Container,
+	Header,
+	LogoImg,
+	Title,
+	MenuContainer,
+	MenuItemLink,
+} from './styles';
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+					<Header>
+						<LogoImg src={logoImg} alt="Logo Minha Carteira" />
+						<Title>Minha Carteira</Title>
+					</Header>
+
+					<MenuContainer>
+						<MenuItemLink href="#">
+							<MdDashboard />
+							Dashboard
+						</MenuItemLink>
+
+						<MenuItemLink href="#">
+							<MdArrowUpward />
+							Entradas
+						</MenuItemLink>
+
+						<MenuItemLink href="#">
+							<MdArrowDownward />
+							Saídas
+						</MenuItemLink>
+
+						<MenuItemLink href="#">
+							<MdExitToApp />
+							LogOut
+						</MenuItemLink>
+					</MenuContainer>
         </Container>
     );
 }
